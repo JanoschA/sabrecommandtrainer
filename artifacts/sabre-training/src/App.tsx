d4e7981fa3c1d1a2b0ca3012b480239bc9227ca0
Janosch Adams <janosch.adams1993@gmail.com>
2026-04-23 23:25:28 +0200

    removed password protection

--- artifacts/sabre-training/src/App.tsx
+++ artifacts/sabre-training/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import PasswordGate from "@/components/PasswordGate";
 import NotFound from "@/pages/not-found";
 
 import Home from "@/pages/Home";
@@ -45,16 +44,14 @@ function Router() {
 
 function App() {
   return (
-    <PasswordGate>
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-            <Router />
-          </WouterRouter>
-          <Toaster />
-        </TooltipProvider>
-      </QueryClientProvider>
-    </PasswordGate>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <Router />
+        </WouterRouter>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
